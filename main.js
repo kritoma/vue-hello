243fbbf40a6be476d0d78a270bf82b607964dc8e
kritoma <toma.kristian97@gmail.com>
2022-05-19 14:24:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = new Vue(
+    {
+        el: "#app",
+        data: {
+            text: "Vue hello",
+            img: "https://cdn3.vectorstock.com/i/1000x1000/35/52/placeholder-rgb-color-icon-vector-32173552.jpg"
+        }
+    }
+)
